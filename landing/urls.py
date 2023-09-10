ff8e4ce808de5f9c32d6ab9323c9618984afcef3
Bijay Khadka <bk006822@gmail.com>
2023-09-10 22:02:00 +0545

    Authentication Initialization | Template setup

--- landing/urls.py
+++ landing/urls.py
@@ -1,4 +1,4 @@
-"""socialmain URL Configuration
+"""landing URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from landing.views import Index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('landing.urls')),
-    path('accounts/', include('allauth.urls')),
-    path("__debug__/", include("debug_toolbar.urls")),
+    path("", Index.as_view(), name="index"),
 ]
