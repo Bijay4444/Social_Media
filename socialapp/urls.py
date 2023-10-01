a8c3b6c9f7f047010aff1a8835a867033d87002e
Bijay Khadka <bk006822@gmail.com>
2023-10-01 21:57:39 +0545

    Finished Basic Messaing System

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -44,5 +44,7 @@ urlpatterns = [
     path('notification/delete/<int:notification_pk>', views.RemoveNotification.as_view(), name='notification_delete'),
     
     path('inbox/', views.ListThreads.as_view(), name='inbox'),
-    path('inbox/create-thread', views.CreateThread.as_view(), name='create-thread'),
+    path('inbox/create-thread/', views.CreateThread.as_view(), name='create-thread'),
+    path('inbox/<int:pk>/', views.ThreadView.as_view(), name='thread'),
+    path('inbox/<int:pk>/create-message/', views.CreateMessage.as_view(), name='create-message'),
 ]
